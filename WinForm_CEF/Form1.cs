30a754d5f7cce595f0fdc489bf4e194c696c457e
Honsinsin <fx130903@163.com>
2019-02-22 22:21:26 +0800

    cefsharp demo

---
+++ WinForm_CEF/Form1.cs
@@ -0,0 +1,34 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm_CEF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         ChromiumWebBrowser browser;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 避免页面加载不出来, 建议加上这句
+            Cef.Initialize(new CefSettings());
+
+            browser = new ChromiumWebBrowser("www.bing.com")
+            {
+                Dock = DockStyle.Fill
+            };
+            this.Controls.Add(browser);
+        }
+    }
+}
